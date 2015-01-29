88cc67eba8b5162d3397bea6a4d45b55e2eab0d1
Andrea Law <alaw@pivotal.io>
2015-01-29 12:31:57 -0500

    Cleanup ci/provision-cf.sh
    Signed-off-by: Graeme Davison <gdavison@pivotal.io>

--- ci/provision-cf.sh
+++ ci/provision-cf.sh
@@ -6,28 +6,39 @@ source $(dirname $0)/test_helpers.sh
 
 trap cleanup EXIT
 
-clean_vagrant
+main() {
+  clean_vagrant
+  setup_directories
+  set_vagrant_home
+  setup_private_routing
+  wget -N https://s3.amazonaws.com/bosh-lite-ci-pipeline/bosh-lite-${BOX_TYPE}-ubuntu-trusty-${BOSH_LITE_CANDIDATE_BUILD_NUMBER}.box
+  box_add_and_vagrant_up $BOX_TYPE $PROVIDER $BOSH_LITE_CANDIDATE_BUILD_NUMBER
 
-export VAGRANT_HOME=/var/vcap/data/.vagrant.d
+  bin/provision_cf
+}
 
-if [ ! -d '../cf-release' ]; then
-  git clone --depth=1 https://github.com/cloudfoundry/cf-release.git ../cf-release
-fi
+setup_private_routing() {
+  sed -e "s/BOSH_LITE_CANDIDATE_BUILD_NUMBER/$BOSH_LITE_CANDIDATE_BUILD_NUMBER/" ci/Vagrantfile.virtualbox > Vagrantfile
+  sed -i'' -e "s/PRIVATE_NETWORK_IP/192.168.50.4/" Vagrantfile
+  cat Vagrantfile
+  sed -i'' -e "s/192.168.50.4/$PRIVATE_NETWORK_IP/" bin/add-route
+  cat bin/add-route
 
-ln -sf $PWD ../bosh-lite
+  set +e
+  bin/add-route
+  set -e
+}
 
-sed -e "s/BOSH_LITE_CANDIDATE_BUILD_NUMBER/$BOSH_LITE_CANDIDATE_BUILD_NUMBER/" ci/Vagrantfile.virtualbox > Vagrantfile
-sed -i'' -e "s/PRIVATE_NETWORK_IP/192.168.50.4/" Vagrantfile
-cat Vagrantfile
-sed -i'' -e "s/192.168.50.4/$PRIVATE_NETWORK_IP/" bin/add-route
-cat bin/add-route
+setup_directories() {
+  if [ ! -d '../cf-release' ]; then
+    git clone --depth=1 https://github.com/cloudfoundry/cf-release.git ../cf-release
+  fi
 
-wget -N https://s3.amazonaws.com/bosh-lite-ci-pipeline/bosh-lite-${BOX_TYPE}-ubuntu-trusty-${BOSH_LITE_CANDIDATE_BUILD_NUMBER}.box
-box_add_and_vagrant_up $BOX_TYPE $PROVIDER $BOSH_LITE_CANDIDATE_BUILD_NUMBER
+  ln -sf $PWD ../bosh-lite
+}
 
-set +e
-bin/add-route
-set -e
+set_vagrant_home() {
+  export VAGRANT_HOME=/var/vcap/data/.vagrant.d
+}
 
-bin/provision_cf
-
+main
